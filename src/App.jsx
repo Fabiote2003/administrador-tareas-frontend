c61df2917084686e7f3064ab014948deb6956de3
Fabio <fabiote2003@gmail.com>
2023-03-09 18:44:07 -0300

    Empezando a sincronizar el backend con el frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+//Layout
+import PrivateRoute from './layout/PrivateRoute';
+import AuthLayout from "./layout/AuthLayout";
+
+//Pages
+import Login from "./pages/Login";
+import Trabajos from "./pages/Trabajos";
+import Register from './pages/Register'
+import Perfil from "./pages/Perfil";
+import NuevoTrabajo from "./pages/NuevoTrabajo";
+import Trabajo from "./pages/Trabajo";
+import EditarTrabajo from "./pages/EditarTrabajo";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+   <>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<AuthLayout />}>
+              <Route index element={<Login />}/> 
+              <Route path="/registro" element={<Register />} />
+        </Route>
+
+        <Route path="/trabajos" element={<PrivateRoute />}>
+              <Route index element={<Trabajos />}/> 
+              <Route path="/trabajos/perfil" element={<Perfil />} />
+              <Route path="/trabajos/:id" element={<Trabajo />} />
+              <Route path="/trabajos/nuevo-trabajo" element={<NuevoTrabajo />} />
+              <Route path="/trabajos/editar-trabajo/:id" element={<EditarTrabajo />}/>
+        </Route>
+        
+      </Routes>
+    </BrowserRouter>
+   </>
   )
 }
 
